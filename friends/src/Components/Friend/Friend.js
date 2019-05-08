42eae31466d41903da9e9dfb5f1079551391a8c4
josh182014 <josh182014@gmail.com>
2019-05-08 16:21:44 -0600

    corrected update functionality so you could change only the part of the friend you wanted without overwriting the entire friend

--- friends/src/Components/Friend/Friend.js
+++ friends/src/Components/Friend/Friend.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
 function Friend(props) {
+    let updatedFriendInfo = {}
+    const updatedFriend = () => {
+        if (props.formData.name.length > 0 ) {
+            updatedFriendInfo.name = props.formData.name
+        }
+        else {
+            updatedFriendInfo.name = props.friend.name
+        }
+        if (props.formData.age.length > 0 ) {
+            updatedFriendInfo.age = props.formData.age
+        }
+        else {
+            updatedFriendInfo.age = props.friend.age
+        }
+        if (props.formData.email.length > 0 ) {
+            updatedFriendInfo.email = props.formData.email
+        }
+        else {
+            updatedFriendInfo.email = props.friend.email
+        }
+    }
+    
     const updateFriend = () => {
-        props.updateFriend(props.friend.id, props.formData)
+        props.updateFriend(props.friend.id, updatedFriendInfo)
         props.UpdateFormHandler()
     }
 
     const deleteFriend = () => {
         props.deleteFriend(props.friend.id)
     }
-
+    updatedFriend()
     return (
     <div>
         Name: <strong>{props.friend.name}</strong> Age: {props.friend.age}
